4cd9dba2c177ce193c66413295618e2714317b7b
Ajmal Abdul Majeed <ajough18@gmail.com>
2024-01-23 15:13:16 +0530

    serialize_public_key(PK)

--- KeyExchange.py
+++ KeyExchange.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 def generate_keypair():
     """
@@ -9,4 +10,13 @@ def generate_keypair():
         backend=default_backend(),
         public_exponent=65537,
         key_size=2048
+    )
+
+def serialize_public_key(public_key):
+    """
+    Serialize the public key to send it over the network
+    """
+    return public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
