403a9823c605ddb09290c8a3eab153a67bceff6d
Ajmal Abdul Majeed <ajough18@gmail.com>
2024-01-23 15:18:56 +0530

    encrypt_with_public_key(PK, PT)

--- KeyExchange.py
+++ KeyExchange.py
@@ -1,6 +1,8 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
 def generate_keypair():
     """
@@ -19,4 +21,17 @@ def serialize_public_key(public_key):
     return public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+def encrypt_with_public_key(public_key, plaintext):
+    """
+    Encrypt the plaintext with the public key
+    """
+    encrypted = public_key.encrypt(
+        plaintext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
     )
